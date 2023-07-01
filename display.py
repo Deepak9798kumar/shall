d9aaeb11c6bd1f4d52cd0b511dd2a5ea90b8032d
Deepak9798kumar <111009426+Deepak9798kumar@users.noreply.github.com>
2023-07-01 18:14:02 +0530

    Update display.py

--- display.py
+++ display.py
@@ -5,22 +5,15 @@ import tkinter as tk
 app = Flask(__name__)
 
 def run_tkinter_app():
-    # Create and configure the Tkinter root window
     root = tk.Tk()
     root.attributes("-fullscreen", True, "-topmost", True)
     root.overrideredirect(True)
 
-    # Hide the cursor
     root.config(cursor="none")
 
-    # Capture the screen (optional, requires pyautogui)
-    # screen = pyautogui.screenshot()
-
-    # Create a label with the captured screen image
     label = tk.Label(root)
     label.place(x=0, y=0, relwidth=1, relheight=1)
 
-    # Add "Hello Moto" label in the center
     hello_label = tk.Label(root, text="Hello Moto", font=("Arial", 24))
     hello_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
@@ -31,16 +24,16 @@ def run_tkinter_app():
 
 @app.route('/')
 def index():
-    return 'Flask app is running'
-
+    return 
 if __name__ == '__main__':
-    # Create a new thread for running the Tkinter app
     tkinter_thread = Thread(target=run_tkinter_app)
     tkinter_thread.daemon = True
     tkinter_thread.start()
 
-    # Run the Flask app
     app.run(host='0.0.0.0', port=5000)
+
+
+
 
 
 
